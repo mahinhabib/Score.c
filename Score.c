586a0398643729188bfa9c608ef6f7bd59d9eadb
Muntasir H Mahin <96934106+mahinhabib@users.noreply.github.com>
2022-02-08 01:17:30 +0600

    Add files via upload

---
+++ Score.c
@@ -0,0 +1,20 @@
+#include <cs50.h> 
+#include <stdio.h> 
+
+int main(void)
+
+{
+    
+    int score1 = get_int("score1 :");
+   
+    printf("score1 :");
+  
+    {
+      for (int i = 0; i < ; i++)
+        
+        printf("@");
+    }
+
+        printf("\n");
+ 
+}
